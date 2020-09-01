892b652e906242681001205b3146fb15d3233173
Andres D' Amelio <andresdamelio24@gmail.com>
2020-08-31 23:33:44 -0400

    Configuration Game

--- src/components/Configuration.vue
+++ src/components/Configuration.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12" v-show="loading">Cargando...</div>
-      <div class="col-12 mb-4" v-show="!loading">
+      <div class="col-12 mb-5">
         <h1 class="instructions-title text-center">¡Bienvenido al juego de las monedas!</h1>
       </div>
-      <div class="col-6" v-show="!loading">
+
+      <div class="col-12 col-sm-12 col-md-6">
         <p class="instructions">
           En este juego tu y tus compañeros representan una
           fábrica que procesa monedas de una manera muy sencilla: Dando vuelta monedas.
@@ -19,125 +19,99 @@
           como impacta en la eficiencia de la operación.
         </p>
       </div>
-      <div class="col-5 offset-1 text-center" v-if="!loading">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span>Iniciar nueva partida</span>
+
+      <div class="col-12 col-sm-12 col-md-5 offset-md-1">
+        <div class="card rounded">
+          <div class="card-header text-center" v-if="!rounds_generated">Iniciar nueva partida</div>
+          <div class="card-header text-center" v-else>
+            <h5 class="mb-0">Iteraciones en Penny Game</h5>
+            <small>El juego se lleva a cabo en cuatro iteraciones.</small>
           </div>
-          <div v-if="!rounds_generated">
-            <el-form :model="form" ref="form" :rules="rules">
-              <el-form-item prop="number_of_players" label="Número de jugadores">
-                <el-input-number
-                  v-model="form.number_of_players"
-                  controls-position="right"
-                  :min="min_number_of_players"
-                  :max="max_number_of_players"
-                ></el-input-number>
-              </el-form-item>
-              <el-form-item prop="total_number_of_coins" label="Número total de monedas">
-                <el-input-number
-                  v-model="form.total_number_of_coins"
-                  controls-position="right"
-                  :min="min_number_of_coins"
-                ></el-input-number>
-              </el-form-item>
-              <el-form-item class="button-bottom">
-                <el-button type="primary" round @click="generateRounds">Generar Rondas</el-button>
-              </el-form-item>
-            </el-form>
+          <div class="card-body" v-if="!rounds_generated">
+            <div class="form-group">
+              <label for="numberPlayers">Número de jugadores</label>
+              <input
+                type="number"
+                class="form-control"
+                id="numberPlayers"
+                min="2"
+                max="5"
+                v-model.number="numberOfPlayers"
+                placeholder="Indique el nro de jugadores"
+              />
+              <small
+                class="form-text text-danger"
+                v-if="showRule"
+              >El número de jugadores debe ser mayor a 1 y menor o igual que 5</small>
+            </div>
+            <div class="text-right">
+              <button class="btn btn-primary rounded" @click="generateRounds" :disabled="showRule">Generar Rondas</button>
+            </div>
           </div>
-          <div v-else>
-            <h5>{{rounds.length}} Rondas</h5>
+          <div class="card-body text-center" v-else>
             <p class="card-text" v-for="(round, index) in rounds" :key="round.key">
-              <span
-                v-if="round.number_of_coins > 1"
-              >Lotes de {{round.number_of_coins}} monedas en la ronda {{index + 1}}</span>
-              <span v-else>Lotes de {{round.number_of_coins}} moneda en la ronda {{index + 1}}</span>
+              <span>Iteración {{index + 1}} - Lote de {{round.coins}} monedas</span>
             </p>
-            <el-button type="primary" @click="play">Jugar</el-button>
+            <button class="btn btn-primary rounded" @click="play">Comenzar</button>
           </div>
-        </el-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const rounds = [
+  { key: 20, coins: 20 },
+  { key: 10, coins: 10 },
+  { key: 5, coins: 5 },
+  { key: 1, coins: 1 }
+];
+
 export default {
   name: "Configuration",
   data: () => {
     return {
-      loading: false,
+      numberOfPlayers: 2,
+      showRule: false,
+      rounds: rounds,
       rounds_generated: false,
-      max_number_of_players: 5,
-      min_number_of_players: 2,
-      min_number_of_coins: 20,
-      rounds: [],
-      form: {
-        number_of_players: 1,
-        total_number_of_coins: 20
-      },
-      rules: {
-        number_of_players: [
-          {
-            required: true,
-            message: "Debes ingresar el número de jugadores",
-            trigger: ["change", "blur"]
-          },
-          {
-            type: "integer",
-            min: 1,
-            max: 5,
-            message:
-              "El número de jugadores debe ser mayor a 1 y menor o igual que 5",
-            trigger: ["change", "blur"]
-          }
-        ]
-      }
     };
   },
   methods: {
+    validate() {
+      if (
+        !this.numberOfPlayers ||
+        ![2, 3, 4, 5].includes(this.numberOfPlayers)
+      ) {
+        this.showRule = true;
+      } else {
+        this.showRule = false;
+      }
+    },
+
     removeRound(round) {
       const roundIndex = this.rounds.indexOf(round);
       if (roundIndex !== -1) {
         this.rounds.splice(roundIndex, 1);
       }
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
+
     generateRounds() {
-      this.loading = true;
-      this.rounds_generated = false;
-      this.rounds = [];
-      this.rounds.push({
-        key: this.form.total_number_of_coins,
-        number_of_coins: this.form.total_number_of_coins
-      });
-      let i = 2;
-      let quotient = Math.floor(this.form.total_number_of_coins / i);
-      let remainder = Math.floor(this.form.total_number_of_coins % i);
-      while (quotient > 1) {
-        quotient = Math.floor(this.form.total_number_of_coins / i);
-        remainder = Math.floor(this.form.total_number_of_coins % i);
-        if (remainder === 0) {
-          this.rounds.push({
-            key: quotient,
-            number_of_coins: quotient
-          });
-        }
-        i = i + 2;
-      }
       this.rounds_generated = true;
-      this.loading = false;
+      console.log("Rondas", this.rounds);
     },
+
     play() {
       this.$emit("play", {
         rounds: this.rounds,
-        number_of_players: this.form.number_of_players,
-        total_number_of_coins: this.form.total_number_of_coins
+        number_of_players: this.numberOfPlayers,
+        total_number_of_coins: 20
       });
     }
+  },
+  watch: {
+    numberOfPlayers: "validate"
   }
 };
 </script>
@@ -152,10 +126,10 @@ export default {
   text-align: justify;
   color: #606266;
 }
-.button-bottom{
-    position: absolute;
-    right: 0;
-    position: absolute;
-    width: 100%;
+.button-bottom {
+  position: absolute;
+  right: 0;
+  position: absolute;
+  width: 100%;
 }
 </style>
