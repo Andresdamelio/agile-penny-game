1ea64d000339f670ecf7a4a21ccd666255bbb400
Andres D' Amelio <andresdamelio24@gmail.com>
2020-09-03 01:40:17 -0400

    Join room, set current player

--- src/modules/penny-game.js
+++ src/modules/penny-game.js
@@ -3,6 +3,8 @@ const pennyModule = {
     rooms: [],
     room: null,
     magigLink: null,
+    isThereRoom: false,
+    currentPlayer: null,
   },
   mutations: {
     ADD_ROOM: (state, room) => {
@@ -10,9 +12,13 @@ const pennyModule = {
     },
     SET_ROOM: (state, room) => {
       state.room = room;
+      state.isThereRoom = true;
     },
     SET_MAGIG_LINK: (state, roomId) => {
       state.magigLink = new URL(`/room/${roomId}`, window.location.origin).href;
+    },
+    SET_CURRENT_PLAYER: (state, id) => {
+      state.currentPlayer = id;
     },
     SOCKET_INIT_ROUND: (state, actualRound) => {
       console.log("Ha iniciado una ronda", state, actualRound);
@@ -24,46 +30,56 @@ const pennyModule = {
     },
     getGame: (state) => {
       return {
-        rounds: state.room.rounds,
-        players: state.room.size,
+        rounds: state.room?.rounds ? state.room.rounds : [],
+        players: state.room?.size ? state.room.size : 2,
         coins: 20,
       };
     },
+    isThereRoom: (state) => {
+      return state.isThereRoom;
+    },
   },
   actions: {
-    socket_new_room: ({ rootState, commit }, { name, players }) => {
+    socket_new_room: ({ rootState, commit, dispatch }, { name, players }) => {
       rootState.io.emit("newRoom", players, (resp) => {
+        const { room } = resp;
         if (resp.ok) {
-          commit("SET_MAGIG_LINK", resp.room.id);
-          rootState.io.emit(
-            "joinRoom",
-            { name, roomId: resp.room.id },
-            (resp) => {
-              if (resp.ok) {
-                commit("ADD_ROOM", resp.room);
-                commit("SET_ROOM", resp.room);
-              }
-            }
-          );
+          commit("ADD_ROOM", room);
+          commit("SET_MAGIG_LINK", room.id);
+          dispatch("socket_join_room", { name, roomId: room.id });
         } else {
           console.error("Algo salió mal, no se pudo crear la sala");
         }
       });
     },
+
+    socket_join_room: ({ rootState, commit }, { name, roomId }) => {
+      rootState.io.emit("joinRoom", { name, roomId }, (resp) => {
+        const { room } = resp;
+        if (resp.ok) {
+          commit("SET_ROOM", room);
+          commit(
+            "SET_CURRENT_PLAYER",
+            room.players[room.players.length - 1].id
+          );
+        } else {
+          console.log("Algo salió mal, recargue la página");
+        }
+      });
+    },
+
     socket_init_round: ({ rootState, state }) => {
       rootState.io.emit("initRound", state.room.id);
     },
 
     get_room_by_id: ({ commit }, id) => {
       fetch(`${process.env.VUE_APP_URL}/room/${id}`)
-        .then((resp) => {
-          return resp.json();
-        })
+        .then((resp) => resp.json())
         .then((resp) => {
           if (resp.ok) {
             commit("SET_ROOM", resp.room);
           } else {
-            console.log("Ha ocurrido un error");
+            console.log("Algo salió mal, recargue la página");
           }
         });
     },
