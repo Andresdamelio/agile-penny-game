2a446b40078dd7cccd6fb3e27550e0ea130a45fe
Andres D' Amelio <andresdamelio24@gmail.com>
2020-09-07 21:09:48 -0400

    remove elemeent-ui, GameIndex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import lang from "element-ui/lib/locale/lang/es";
-import locale from "element-ui/lib/locale";
 
 Vue.config.productionTip = false;
 
@@ -10,12 +8,6 @@ import store from "./store.js";
 
 // Vue Router
 import router from "./router";
-
-/* Configure Element Ui Language*/
-locale.use(lang);
-
-// Globally Registered Components
-import "./globalComponents.js";
 
 new Vue({
   store,
